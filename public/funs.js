fb50664699ac1349b014e9655ee3737b26780562
Travis Hardiman <dieseltravis@users.noreply.github.com>
2021-11-20 02:56:47 -0500

    init

---
+++ public/funs.js
@@ -0,0 +1,154 @@
+(function () {
+  'use strict';
+
+  const all = {
+    day1: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day2: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day3: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day4: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day5: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day6: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day7: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day8: {
+      part1: data => {
+      }
+    },
+    day9: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day10: {
+      part1: data => {
+      }
+    },
+    day11: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day12: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day13: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day14: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day15: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day16: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day17: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day18: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day19: {
+      part1: data => {
+      },
+      part2: () => {
+      }
+    },
+    day20: {
+      part1: data => {
+      },
+      part2: () => {
+      }
+    },
+    day21: {
+      part1: data => {
+      },
+      part2: () => {
+      }
+    },
+    day22: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day23: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day24: {
+      part1: data => {
+      },
+      part2: data => {
+      }
+    },
+    day25: {
+      part1: data => {
+      },
+      part2: () => {
+      }
+    }
+  };
+
+  this.funs = (day, part) => all['day' + day]['part' + part];
+}.call(this));

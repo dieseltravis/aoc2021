41dc9fd44aa997bad1f062acd9e942192378ade7
Glitch (valiant-nervous-yoke) <none>
2021-11-21 07:13:54 +0000

    🚁🍍 Updated with Glitch: eslint

--- public/funs.js
+++ public/funs.js
@@ -1,154 +1,108 @@
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
   const all = {
     day1: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day2: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day3: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day4: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day5: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day6: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day7: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day8: {
-      part1: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day9: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day10: {
-      part1: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day11: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day12: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day13: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day14: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day15: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day16: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day17: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day18: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day19: {
-      part1: data => {
-      },
-      part2: () => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day20: {
-      part1: data => {
-      },
-      part2: () => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day21: {
-      part1: data => {
-      },
-      part2: () => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day22: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day23: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day24: {
-      part1: data => {
-      },
-      part2: data => {
-      }
+      part1: () => {},
+      part2: () => {}
     },
     day25: {
-      part1: data => {
-      },
-      part2: () => {
-      }
+      part1: () => {},
+      part2: () => {}
     }
   };
 
-  this.funs = (day, part) => all['day' + day]['part' + part];
+  this.funs = (day, part) => all["day" + day]["part" + part];
 }.call(this));

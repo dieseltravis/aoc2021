bc45d86080747507a02b57f359d65c670237cfa7
Glitch (valiant-nervous-yoke) <none>
2021-12-04 03:01:48 +0000

    🚒🐁 Updated with Glitch: d3p1 clean-up

--- public/funs.js
+++ public/funs.js
@@ -86,26 +86,26 @@
         const bits = data.trim().split('\n').map(word => word.split('').map(Number));
         let common = null;
         const bitLength = bits.length;
-        for(let i = 0; i < bitLength; i++) {
+        for (let i = 0; i < bitLength; i++) {
           const word = bits[i];
           const wordLength = word.length;
           if (i === 0) {
-            common = Array.from({length: wordLength}, () => [0,0]);
+            common = Array.from({ length: wordLength }, () => [0, 0]);
           }
-          for(let j = 0; j < wordLength; j++) {
+          for (let j = 0; j < wordLength; j++) {
             common[j][word[j]] += 1;
           }
         }
         console.log(common);
-        const maxs = common.reduce((acc, item, i) => { 
-          acc[i] = item.indexOf(Math.max.apply(Math, item)); 
-          return acc; 
+        const maxs = common.reduce((acc, item, i) => {
+          acc[i] = item.indexOf(Math.max.apply(Math, item));
+          return acc;
         }, []);
         console.log(maxs);
         const gamma = parseInt(maxs.join(''), 2);
-        const mins = common.reduce((acc, item, i) => { 
-          acc[i] = item.indexOf(Math.min.apply(Math, item)); 
-          return acc; 
+        const mins = common.reduce((acc, item, i) => {
+          acc[i] = item.indexOf(Math.min.apply(Math, item));
+          return acc;
         }, []);
         console.log(mins);
         const epsilon = parseInt(mins.join(''), 2);
